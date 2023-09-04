02311509d83e7c1f218bcba22064b84d0209af55
gaw1ik <64555628+gaw1ik@users.noreply.github.com>
2023-09-04 17:17:48 -0500

    Landing Page 01
    Updating the site to function more as a landing page

--- js/main.js
+++ js/main.js
@@ -1,33 +1,33 @@
 
-versionNumber = "1.07b";
-console.log("version ", versionNumber);
+// versionNumber = "1.07b";
+// console.log("version ", versionNumber);
 
 //window.addEventListener('DOMContentLoaded', () => {
-borderOffProperty = "1px solid rgba(100, 100, 100, 0.0)";
-borderOnProperty  = "1px solid white";
+// borderOffProperty = "1px solid rgba(100, 100, 100, 0.0)";
+// borderOnProperty  = "1px solid white";
 
-pageButtons = document.getElementsByClassName("pageButton");
-pageContainers = document.getElementsByClassName("pageContainer");
+// pageButtons = document.getElementsByClassName("pageButton");
+// pageContainers = document.getElementsByClassName("pageContainer");
 
-for(let i=0; i<pageContainers.length; i++) {
-    pageContainers[i].style.display = "none";
-    pageButtons[i].style.borderBottom = borderOffProperty;
-}
-
-
-navToggleOn = -1;
+// for(let i=0; i<pageContainers.length; i++) {
+//     pageContainers[i].style.display = "none";
+//     pageButtons[i].style.borderBottom = borderOffProperty;
+// }
 
 
+// navToggleOn = -1;
 
 
 
 
-window.onload = function () {
 
 
-    // get elements for the pageButtons and pageContainers
-    pageButtons = document.getElementsByClassName("pageButton");
-    pageContainers = document.getElementsByClassName("pageContainer");
+// window.onload = function () {
+
+
+//     // get elements for the pageButtons and pageContainers
+//     pageButtons = document.getElementsByClassName("pageButton");
+//     pageContainers = document.getElementsByClassName("pageContainer");
 
 
     
@@ -36,38 +36,38 @@ window.onload = function () {
 
 
 
-    // when you first open the page you'll want to mute all pages except for one.
-    for(let i=0; i<pageContainers.length; i++) {
-        pageContainers[i].style.display = "none";
-        pageButtons[i].style.borderBottom = borderOffProperty;
-    }
+//     // when you first open the page you'll want to mute all pages except for one.
+//     for(let i=0; i<pageContainers.length; i++) {
+//         pageContainers[i].style.display = "none";
+//         pageButtons[i].style.borderBottom = borderOffProperty;
+//     }
 
-    let defaultPageIndex = 0;
+//     let defaultPageIndex = 0;
 
-    pageContainers[defaultPageIndex].style.display = "block";
-    pageButtons[defaultPageIndex].style.borderBottom = borderOnProperty;
+//     pageContainers[defaultPageIndex].style.display = "block";
+//     pageButtons[defaultPageIndex].style.borderBottom = borderOnProperty;
 
 
 
-    // add the pageButton event listeners
-    for(let i=0; i<pageButtons.length; i++) {
+//     // add the pageButton event listeners
+//     for(let i=0; i<pageButtons.length; i++) {
 
-        pageButtons[i].addEventListener( "click", onPageButtonClick );
+//         pageButtons[i].addEventListener( "click", onPageButtonClick );
     
-    }
+//     }
 
 
 
 
 
 
-    // Nav Toggle Stuff
-    navToggleButton = document.getElementById("navToggleButton");
-    navbar = document.getElementById("navbar");
-    navToggleButton.addEventListener( "click", onNavToggleButtonClick );
+//     // Nav Toggle Stuff
+//     navToggleButton = document.getElementById("navToggleButton");
+//     navbar = document.getElementById("navbar");
+//     navToggleButton.addEventListener( "click", onNavToggleButtonClick );
     
 
-};
+// };
 
 
 
@@ -78,55 +78,55 @@ window.onload = function () {
 
 
 
-function onPageButtonClick() {
+// function onPageButtonClick() {
 
-    //console.log(this.id);
+//     //console.log(this.id);
 
-    for(let i=0; i<pageContainers.length; i++) {
-        pageContainers[i].style.display = "none";
-        pageButtons[i].style.borderBottom = borderOffProperty;
-    }
+//     for(let i=0; i<pageContainers.length; i++) {
+//         pageContainers[i].style.display = "none";
+//         pageButtons[i].style.borderBottom = borderOffProperty;
+//     }
 
-    let pageIndex = getNamedIndexOfElement("Page", this);
+//     let pageIndex = getNamedIndexOfElement("Page", this);
 
-    // // for changing image heights depending on the page selected
-    // if(pageIndex==1) {
-    //     document.documentElement.style.setProperty('--image-height', '70vh');
-    // }
+//     // // for changing image heights depending on the page selected
+//     // if(pageIndex==1) {
+//     //     document.documentElement.style.setProperty('--image-height', '70vh');
+//     // }
 
-    pageContainers[pageIndex].style.display = "block";
-    pageButtons[pageIndex].style.borderBottom = borderOnProperty;
+//     pageContainers[pageIndex].style.display = "block";
+//     pageButtons[pageIndex].style.borderBottom = borderOnProperty;
 
-    onNavToggleButtonClick();
+//     onNavToggleButtonClick();
 
-}
-
-
+// }
 
 
 
 
 
-function onNavToggleButtonClick() {
 
-    if( navToggleOn == 1 ) {
 
-        // navbar.style.display = "none";
-        navbar.className = "navbarClosed";
+// function onNavToggleButtonClick() {
 
-    } else if ( navToggleOn == -1 ) {
+//     if( navToggleOn == 1 ) {
 
-        // navbar.style.display = "flex";
-        navbar.className = "navbarOpen";
+//         // navbar.style.display = "none";
+//         navbar.className = "navbarClosed";
 
-    }
+//     } else if ( navToggleOn == -1 ) {
 
-    // flip the value of navToggleOn
-    navToggleOn = navToggleOn * -1;
+//         // navbar.style.display = "flex";
+//         navbar.className = "navbarOpen";
 
-    //console.log("navToggleOn",navToggleOn)
+//     }
+
+//     // flip the value of navToggleOn
+//     navToggleOn = navToggleOn * -1;
+
+//     //console.log("navToggleOn",navToggleOn)
     
-}
+// }
 
 
 
